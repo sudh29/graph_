7bcd70fa113cc689e67a33e1bb6d236c2efba5db
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-04-29 17:34:02 +0530

    Update 7_Minimum_Step_by_Knight.py

--- 7_Minimum_Step_by_Knight.py
+++ 7_Minimum_Step_by_Knight.py
@@ -5,24 +5,24 @@ class Solution:
 	#Function to find out minimum steps Knight needs to reach target position.
 	def minStepToReachTarget(self, KnightPos, TargetPos, N):
 		dx = [2, 2, -2, -2, 1, 1, -1, -1]
-        dy = [1, -1, 1, -1, 2, -2, 2, -2]
-        
-        queue = []
-        queue.append([KnightPos[0], KnightPos[1], 0])
-        visited = [[False for _ in range(N + 1)] for _ in range(N + 1)]
-        visited[KnightPos[0]][KnightPos[1]] = True
-        
-        while len(queue)>0:
-            x,y,dist = queue.pop(0)
-            if(x == TargetPos[0] and y == TargetPos[1]):
-                return dist
-            for i in range(8):
-                newX = x + dx[i]
-                newY = y + dy[i]
-     
-                if is_valid(newX, newY, N) and not visited[newX][newY]:
-                    visited[newX][newY] = True
-                    queue.append([newX, newY, dist + 1])
+	        dy = [1, -1, 1, -1, 2, -2, 2, -2]
+	        
+	        queue = []
+	        queue.append([KnightPos[0], KnightPos[1], 0])
+	        visited = [[False for _ in range(N + 1)] for _ in range(N + 1)]
+	        visited[KnightPos[0]][KnightPos[1]] = True
+	        
+	        while len(queue)>0:
+	            x,y,dist = queue.pop(0)
+	            if(x == TargetPos[0] and y == TargetPos[1]):
+	                return dist
+	            for i in range(8):
+	                newX = x + dx[i]
+	                newY = y + dy[i]
+	     
+	                if is_valid(newX, newY, N) and not visited[newX][newY]:
+	                    visited[newX][newY] = True
+	                    queue.append([newX, newY, dist + 1])
 
 #{ 
  # Driver Code Starts
